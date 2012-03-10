d8fb5bde7ef4a93359592006d158abb3051205dc
unknown <DAIKI@DAIKI-PC.(none)>
2012-03-10 23:27:32 +0900

    Initial Commit

---
+++ src/killing-time.cpp
@@ -0,0 +1,14 @@
+//============================================================================
+// Name        : killing-time.cpp
+// Author      : Daiki Nihara
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	printf("test");
+	return 0;
+}
